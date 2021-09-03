91e8c1acb8eeefe78698d2d067acaacd5893423d
Rimian Perkins <hello@rimian.com.au>
2021-09-03 23:18:20 +1000

    puppeteer runs

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,11 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should be titled "Google"', async () => {
+    await expect(page.title()).resolves.toMatch('Google');
+  });
 });

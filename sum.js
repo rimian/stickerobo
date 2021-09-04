b3e7aa82865e2d8f8c7ed413b800cbadf2486f52
Rimian Perkins <hello@rimian.com.au>
2021-09-04 11:17:36 +1000

    login

--- sum.js
+++ sum.js
@@ -1,4 +1,21 @@
+const puppeteer = require('puppeteer');
 class Foo {
+  constructor(config) {
+    this.config = config;
+  }
+
+  login() {
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(config.url);
+      await page.type('#ReduxFormInput1', config.username);
+      await page.type('#ReduxFormInput2', config.password);
+      await page.click('submit');
+      await browser.close();
+    })();
+  }
+
 
 }
 
--- sum.test.js
+++ sum.test.js
@@ -1,12 +1,14 @@
 const Foo = require('./sum');
 const config = require('./config');
 
-describe('Google', () => {
-  beforeAll(async () => {
-    await page.goto(config.url);
+describe('Foo', () => {
+  beforeAll(() => {
+
   });
 
-  it('should be titled "Google"', async () => {
-    await expect(page.title()).resolves.toMatch('Log In | Redbubble');
+  it('should be', () => {
+    const foo = new Foo(config);
+    foo.login();
+
   });
 });

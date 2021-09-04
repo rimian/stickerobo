4db7ca68e545ffa3f359b87fdcbd19dd2b64e9ac
Rimian Perkins <hello@rimian.com.au>
2021-09-04 14:02:29 +1000

    jest-fetch-mock example

---
+++ api.test.js
@@ -0,0 +1,20 @@
+const APIRequest = require('./api');
+require('jest-fetch-mock').enableMocks()
+
+describe('testing api', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+
+  it('calls google and returns data to me', async () => {
+    fetch.mockResponseOnce(JSON.stringify({ data: '12345' }))
+
+    //assert on the response
+    const res = await APIRequest('google')
+    expect(res.data).toEqual('12345')
+
+    //assert on the times called and arguments given to fetch
+    expect(fetch.mock.calls.length).toEqual(1)
+    expect(fetch.mock.calls[0][0]).toEqual('https://google.com')
+  })
+})

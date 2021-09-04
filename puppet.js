9b0232b7b5ece58cc93ce76fa9e9aca375e7084e
Rimian Perkins <hello@rimian.com.au>
2021-09-04 16:13:34 +1000

    intercept request

--- puppet.js
+++ puppet.js
@@ -1,6 +1,12 @@
 
+const fetchRequest = function(who) {
+  return fetch(who).then(res => res.json())
+}
+
+module.exports = fetchRequest;
+
 const puppetRequest = function(who) {
-  return fetch(who)
+  return fetch(who).then(res => res.json())
 }
 
 module.exports = puppetRequest;
--- puppet.test.js
+++ puppet.test.js
@@ -1,4 +1,7 @@
+const fetchRequest = require('./puppet');
 const puppetRequest = require('./puppet');
+const puppeteer = require('puppeteer');
+
 require('jest-fetch-mock').enableMocks()
 
 describe('testing api', () => {
@@ -8,10 +11,62 @@ describe('testing api', () => {
   })
 
   it('calls google and returns data to me', () => {
-
-    return puppetRequest('https://google.com').then(res => {
+    return fetchRequest('https://google.com').then(json => {
+      expect(json.data).toBe('12345')
       expect(fetch.mock.calls.length).toEqual(1)
       expect(fetch.mock.calls[0][0]).toEqual('https://google.com')
     })
+  })
+
+  it('calls google and returns data to me', () => {
+
+
+    return (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.setRequestInterception(true);
+
+      page.on('request', request => {
+        request.respond({
+          content: 'application/json',
+          headers: {"Access-Control-Allow-Origin": "*"},
+          body: JSON.stringify({foo: 'bar'})
+        })
+      });
+
+      res = await page.goto('https://example.com');
+      json = await res.json()
+      await browser.close();
+
+      expect(json).toStrictEqual({"foo": "bar"})
+    })();
+
+
+    //   const page = await browser.newPage();
+
+    //   page.goto('https://google.com').then(res => res.json()).then(data => {
+    //     expect(data).toBe(4)
+    //   })
+
+
+
+    // (async () => {
+    //   const browser = await puppeteer.launch();
+    //   const page = await browser.newPage();
+
+    //   page.goto('https://google.com').then(res => res.json()).then(data => {
+    //     expect(data).toBe(4)
+    //   })
+    //   // await page.screenshot({ path: 'example.png' });
+
+    //   await browser.close();
+    // })();
+
+    // return puppetRequest('https://google.com').then(json => {
+    //   expect(json.data).toBe('12345')
+    //   expect(fetch.mock.calls.length).toEqual(1)
+    //   expect(fetch.mock.calls[0][0]).toEqual('https://google.com')
+    // })
   })
 })

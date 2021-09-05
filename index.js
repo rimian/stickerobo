d7ac52644b96f811b8765fe18cebaee5847e8fa9
Rimian Perkins <hello@rimian.com.au>
2021-09-05 23:39:21 +1000

    more debugging

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ puppeteer.use(RecaptchaPlugin(config.recaptcha))
 puppeteer.launch({ headless: false }).then(async browser => {
   page = await browser.newPage();
 
+  // await page.setDefaultNavigationTimeout(0);
+
   await page.goto(config.url);
   await page.type('#ReduxFormInput1', config.username);
   await page.type('#ReduxFormInput2', config.password);
   await page.click('.login-form button[type="submit"]');
+
+  const { captchas, solutions, solved, error } = await page.solveRecaptchas()
+
+  console.log(captchas)
+  console.log(solutions)
+  console.log(solved)
+  console.log(error)
+
   await page.waitForNavigation();
-  await page.solveRecaptchas();
-  await page.click('#recaptcha-verify-button');
-  await page.waitForNavigation();
+  await page.screenshot({ path: 'result.png', fullPage: true })
+  await browser.close();
 })

3d14af7bd4e69b3bf57f9fe04a9ed2f255efe426
Rimian Perkins <hello@rimian.com.au>
2021-09-06 21:33:06 +1000

    upload

--- index.js
+++ index.js
@@ -15,11 +15,30 @@ const login = async (page) => {
   await page.waitForNavigation();
 }
 
+const upload = async (page) => {
+  await page.goto('https://www.redbubble.com/portfolio/images/new')
+  const elementHandle = await page.$('#select-image-single');
+  await elementHandle.uploadFile('./hello-world.png');
+  await page.waitForNavigation();
+}
+
+const submit = async (page) => {
+  // #work_safe_for_work_true
+};
+
 puppeteer.launch({ headless: false }).then(async browser => {
   page = await browser.newPage();
 
   await login(page).catch(e => {
     console.log('Login error')
+  });
+
+  await upload(page).catch(e => {
+    console.log('Upload error')
+  });
+
+  await submit(page).catch(e => {
+    console.log('Submit error')
   });
 
   // await page.screenshot({ path: 'result.png', fullPage: true })
